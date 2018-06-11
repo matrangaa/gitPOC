47c74c2f22e4ffb8fa6fa0d0dfe478c2b2e690e2
amatranga <amatranga@mesirowfinancial.com>
2018-06-11 14:22:29 -0500

    add new component

--- client/src/components/HelloWorld.jsx
+++ client/src/components/HelloWorld.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const HelloWorld = () => {
-  return (
-    <h4>HelloWorld component</h4>
-  );
+  return ([
+    <h4>HelloWorld component</h4>,
+    <h5>A new component appears!</h5>
+  ]);
 }
 
 export default HelloWorld;
